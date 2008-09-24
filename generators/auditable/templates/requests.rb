620884a9b4af4a47376daff09fd3aca64f6269af
universal <jhed@informatik.uni-kiel.de>
2008-09-24 16:05:41 +0200

    added search functionality to request-template

--- generators/auditable/templates/requests.rb
+++ generators/auditable/templates/requests.rb
@@ -1,4 +1,42 @@
 class <%= request_class_name %> < ActiveRecord::Base
   belongs_to :session, :class_name => "<%= session_class_name %>", :foreign_key => "<%= session_singular_name %>_id"
   has_many :modifications, :class_name => "<%= modification_class_name %>"
+
+  def self.search(query, options={})
+    with_scope :find => {:conditions => search_conditions(query)} do
+      find :all, options   
+    end
+  end
+  
+  private
+  def self.search_conditions(query)
+    return nil if query.blank?
+    fields = query.scan(/(\w+): ([\w\.]+)/).select{|option| column_names.include? option[0]}
+    time = query.scan(/(start|end): ([\d-]*\s{0,1}[\d:]*)/)
+    binds = {}
+    and_frags = []
+    count = 1
+    fields.each do |field, value|
+      and_frags << "LOWER(#{field}) LIKE :word#{count}"
+      binds["word#{count}".to_sym] = "%#{value.to_s.downcase}%"
+      count += 1
+    end
+    time.each do |field, value|
+      begin 
+        time_point = Time.parse(value)
+      rescue ArgumentError
+      end
+      if time_point
+        case field
+          when "start"
+            and_frags << "`when` > :word#{count}"
+          else
+            and_frags << "`when` < :word#{count}"
+        end
+        binds["word#{count}".to_sym] = time_point
+        count += 1
+      end      
+    end
+    [and_frags.join(" AND "), binds]
+  end
 end
--- init.rb
+++ init.rb
@@ -4,5 +4,7 @@ config.after_initialize do
   ::DEFAULT_AUDIT_LOG = AuditLog.new(File.join(RAILS_ROOT, "log", "#{config.environment}_audit.log"))
   ::DEFAULT_AUDIT_LOG.auto_flushing = false
   ::DEFAULT_AUDIT_LOG.set_non_blocking_io
+  ::DEFAULT_AUDIT_LOG.info("\n")
+  ::DEFAULT_AUDIT_LOG.flush
   require 'hooks/action_controller'
 end
